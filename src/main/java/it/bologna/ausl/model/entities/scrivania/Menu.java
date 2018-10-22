20a747ec8653dbda06132704f7438715ad60abcb
s.fayssel <devnull@localhost>
2018-10-22 16:48:35 +0200

    update

---
+++ src/main/java/it/bologna/ausl/model/entities/scrivania/Menu.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.bologna.ausl.model.entities.scrivania;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import it.bologna.ausl.model.entities.baborg.Azienda;
+import it.bologna.ausl.model.entities.configuration.Applicazione;
+import it.nextsw.common.types.GenericArrayUserType;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Cacheable;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+/**
+ *
+ * @author fayssel
+ */
+@TypeDefs(
+        {
+            @TypeDef(name = "array", typeClass = GenericArrayUserType.class)
+        }
+)
+@Entity
+@Table(name = "menu", catalog = "internauta", schema = "scrivania")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Cacheable(false)
+public class Menu implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @JoinColumn(name = "id_azienda", referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    private Azienda idAzienda;
+    @Basic(optional = false)
+    @NotNull
+    @JoinColumn(name = "id_applicazione", referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    private Applicazione idApplicazione;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 2147483647)
+    @Column(name = "descrizione")
+    private String descrizione;
+    @Size(max = 2147483647)
+    @Column(name = "open_command")
+    private String openCommand;
+
+    public Menu() {
+    }
+
+    public Menu(Integer id) {
+        this.id = id;
+    }
+
+    public Menu(Integer id, Azienda idAzienda, Applicazione idApplicazione, String descrizione) {
+        this.id = id;
+        this.idAzienda = idAzienda;
+        this.idApplicazione = idApplicazione;
+        this.descrizione = descrizione;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Azienda getIdAzienda() {
+        return idAzienda;
+    }
+
+    public void setIdAzienda(Azienda idAzienda) {
+        this.idAzienda = idAzienda;
+    }
+
+    public Applicazione getIdApplicazione() {
+        return idApplicazione;
+    }
+
+    public void setIdApplicazione(Applicazione idApplicazione) {
+        this.idApplicazione = idApplicazione;
+    }
+
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Menu)) {
+            return false;
+        }
+        Menu other = (Menu) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "it.bologna.ausl.model.entities.scrivania.Menu[ id=" + id + " ]";
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getOpenCommand() {
+        return openCommand;
+    }
+
+    public void setOpenCommand(String openCommand) {
+        this.openCommand = openCommand;
+    }
+    
+}
