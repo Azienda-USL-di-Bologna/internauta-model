9700ccaf754a5e7553d834d470fa2ab795e8f6e7
a.marcomini <devnull@localhost>
2019-05-15 16:05:30 +0200

    inserito campo resetLastuidTime su Pec

--- src/main/java/it/bologna/ausl/model/entities/baborg/Pec.java
+++ src/main/java/it/bologna/ausl/model/entities/baborg/Pec.java
@@ -1,6 +1,7 @@
 package it.bologna.ausl.model.entities.baborg;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.querydsl.core.annotations.PropertyType;
 import com.querydsl.core.annotations.QueryType;
@@ -9,6 +10,7 @@ import it.bologna.ausl.model.entities.shpeck.Draft;
 import it.bologna.ausl.model.entities.shpeck.Folder;
 import it.bologna.ausl.model.entities.shpeck.Tag;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
@@ -24,6 +26,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
 
 /**
  *
@@ -35,66 +38,66 @@ import javax.validation.constraints.NotNull;
 public class Pec implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "id")
     protected Integer id;
-    
+
     @Basic(optional = false)
     @NotNull
     @Column(name = "indirizzo", columnDefinition = "text")
     private String indirizzo;
-    
+
     @Basic(optional = false)
     @NotNull
     @Column(name = "username", columnDefinition = "text")
     private String username;
-    
+
     @Basic(optional = false)
     @NotNull
     @Column(name = "password", columnDefinition = "text")
     private String password;
-    
+
     @Basic(optional = false)
     @NotNull
     @Column(name = "attiva")
     private Boolean attiva;
-    
+
     @Basic(optional = false)
     @NotNull
     @Column(name = "message_policy")
     private Integer messagePolicy;
-    
+
     @Basic(optional = false)
     @NotNull
     @Column(name = "per_riservato")
     private Boolean perRiservato;
-    
+
     @Column(name = "descrizione", columnDefinition = "text")
     private String descrizione;
-    
+
     @OneToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, mappedBy = "idPec", fetch = FetchType.LAZY)
     @JsonBackReference(value = "pecStrutturaList")
     private List<PecStruttura> pecStrutturaList;
-    
+
     @JoinColumn(name = "id_azienda_repository", referencedColumnName = "id")
     @ManyToOne(optional = true, fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     private Azienda idAziendaRepository; // da eliminare, trasformato in lista
-    
+
     @OneToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, mappedBy = "idPec", fetch = FetchType.LAZY, orphanRemoval = true)
     @JsonBackReference(value = "pecAziendaList")
     private List<PecAzienda> pecAziendaList;
-    
+
     @JoinColumn(name = "id_pec_provider", referencedColumnName = "id")
     @ManyToOne(optional = false, fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     private PecProvider idPecProvider;
-    
+
     @OneToMany(mappedBy = "idPec", fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST}, orphanRemoval = true)
     @JsonBackReference(value = "pecUtenteList")
     private List<PecUtente> pecUtenteList;
-    
+
     @OneToMany(mappedBy = "idPec", fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST}, orphanRemoval = true)
     @JsonBackReference(value = "tagList")
     private List<Tag> tagList;
@@ -102,36 +105,41 @@ public class Pec implements Serializable {
     @OneToMany(mappedBy = "idPec", fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.PERSIST}, orphanRemoval = true)
     @JsonBackReference(value = "folderList")
     private List<Folder> folderList;
-    
+
     @OneToMany(mappedBy = "idPec", fetch = FetchType.LAZY, orphanRemoval = true)
     @JsonBackReference(value = "draftList")
     private List<Draft> draftList;
-    
+
     @Column(name = "massiva")
     private Boolean massiva;
-    
+
     @Column(name = "chiusa")
     private Boolean chiusa;
-    
+
     @Basic(optional = false)
     @NotNull
     @Column(name = "is_pec")
     private Boolean isPec = false;
-    
+
     @Basic(optional = false)
     @NotNull
     @Column(name = "lastuid")
     private Long lastuid = 0L;
-    
+
     @Basic(optional = false)
     @NotNull
     @Column(name = "send_delay")
     private Integer sendDelay = -1;
-    
+
+    @Column(name = "reset_lastuid_time")
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime resetLastuidTime;
+
     @Transient
     @QueryType(PropertyType.SIMPLE)
     private List<PermessoEntitaStoredProcedure> permessi;
-    
+
     @Transient
     @QueryType(PropertyType.SIMPLE)
     private List<Persona> gestori;
@@ -240,7 +248,7 @@ public class Pec implements Serializable {
     public void setPecAziendaList(List<PecAzienda> pecAziendaList) {
         this.pecAziendaList = pecAziendaList;
     }
-    
+
     public PecProvider getIdPecProvider() {
         return idPecProvider;
     }
@@ -296,7 +304,7 @@ public class Pec implements Serializable {
     public void setChiusa(Boolean chiusa) {
         this.chiusa = chiusa;
     }
-    
+
     public List<PermessoEntitaStoredProcedure> getPermessi() {
         return permessi;
     }
@@ -335,6 +343,14 @@ public class Pec implements Serializable {
 
     public void setSendDelay(Integer sendDelay) {
         this.sendDelay = sendDelay;
+    }
+
+    public LocalDateTime getResetLastuidTime() {
+        return resetLastuidTime;
+    }
+
+    public void setResetLastuidTime(LocalDateTime resetLastuidTime) {
+        this.resetLastuidTime = resetLastuidTime;
     }
 
     @Override
