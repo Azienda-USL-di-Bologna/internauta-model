a67f9d1ccd5d1857b2663cf6d29c801e93d0c12d
f.gusella <devnull@localhost>
2019-07-22 14:59:08 +0200

    messageExtensionList

---
+++ src/main/java/it/bologna/ausl/model/entities/shpeck/MessageExtension.java
@@ -0,0 +1,92 @@
+package it.bologna.ausl.model.entities.shpeck;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author gusgus
+ */
+@Entity
+@Table(name = "messages_extensions", catalog = "internauta", schema = "shpeck")
+public class MessageExtension implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id")
+    private Integer id;
+    @Size(max = 2147483647)
+    @Column(name = "address_from")
+    private String addressFrom;
+    @JoinColumn(name = "id", referencedColumnName = "id", insertable = false, updatable = false)
+    @OneToOne(optional = false, fetch = FetchType.LAZY)
+    private Message message;
+
+    public MessageExtension() {
+    }
+
+    public MessageExtension(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAddressFrom() {
+        return addressFrom;
+    }
+
+    public void setAddressFrom(String addressFrom) {
+        this.addressFrom = addressFrom;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof MessageExtension)) {
+            return false;
+        }
+        MessageExtension other = (MessageExtension) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "it.bologna.ausl.model.entities.shpeck.MessagesExtensions[ id=" + id + " ]";
+    }
+    
+}
