c7e9d84828a7c5ad459b836b88ca3cc7659c15a2
g.demarco <devnull@localhost>
2019-09-10 17:56:34 +0200

    cambiato il campo idUtenti con idPersone

--- src/main/java/it/bologna/ausl/model/entities/messaggero/AmministrazioneMessaggio.java
+++ src/main/java/it/bologna/ausl/model/entities/messaggero/AmministrazioneMessaggio.java
@@ -77,9 +77,9 @@ public class AmministrazioneMessaggio implements Serializable {
     @Type(type = "array", parameters = @Parameter(name = "elements-type", value = GenericArrayUserType.INTEGER_ELEMENT_TYPE))
     private Integer[] idStrutture;
     
-    @Column(name = "id_utenti", columnDefinition = "id_utenti[]")
+    @Column(name = "id_persone", columnDefinition = "id_persone[]")
     @Type(type = "array", parameters = @Parameter(name = "elements-type", value = GenericArrayUserType.INTEGER_ELEMENT_TYPE))
-    private Integer[] idUtenti;
+    private Integer[] idPersone;
    
     @Column(name = "per_tutti")
     private Boolean perTutti;
@@ -134,13 +134,13 @@ public class AmministrazioneMessaggio implements Serializable {
     public AmministrazioneMessaggio() {
     }
 
-    public AmministrazioneMessaggio(String titolo, String testo, String[] idApplicazioni, Integer[] idAziende, Integer[] idStrutture, Integer[] idUtenti, Boolean perTutti, LocalDateTime dataPubblicazione, String invasivita, String tipologia, Integer intervallo, LocalDateTime dataScadenza) {
+    public AmministrazioneMessaggio(String titolo, String testo, String[] idApplicazioni, Integer[] idAziende, Integer[] idStrutture, Integer[] idPersone, Boolean perTutti, LocalDateTime dataPubblicazione, String invasivita, String tipologia, Integer intervallo, LocalDateTime dataScadenza) {
         this.titolo = titolo;
         this.testo = testo;
         this.idApplicazioni = idApplicazioni;
         this.idAziende = idAziende;
         this.idStrutture = idStrutture;
-        this.idUtenti = idUtenti;
+        this.idPersone = idPersone;
         this.perTutti = perTutti;
         this.dataPubblicazione = dataPubblicazione;
         this.invasivita = invasivita;
@@ -197,12 +197,12 @@ public class AmministrazioneMessaggio implements Serializable {
         this.idStrutture = idStrutture;
     }
 
-    public Integer[] getIdUtenti() {
-        return idUtenti;
+    public Integer[] getIdPersone() {
+        return idPersone;
     }
 
-    public void setIdUtenti(Integer[] idUtenti) {
-        this.idUtenti = idUtenti;
+    public void setIdPersone(Integer[] idPersone) {
+        this.idPersone = idPersone;
     }
 
     public Boolean getPerTutti() {
--- src/main/java/it/bologna/ausl/model/entities/messaggero/TemplateMessaggio.java
+++ src/main/java/it/bologna/ausl/model/entities/messaggero/TemplateMessaggio.java
@@ -72,9 +72,9 @@ public class TemplateMessaggio implements Serializable {
     @Type(type = "array", parameters = @Parameter(name = "elements-type", value = GenericArrayUserType.INTEGER_ELEMENT_TYPE))
     private Integer[] idStrutture;
     
-    @Column(name = "id_utenti", columnDefinition = "id_utenti[]")
+    @Column(name = "id_persone", columnDefinition = "id_persone[]")
     @Type(type = "array", parameters = @Parameter(name = "elements-type", value = GenericArrayUserType.INTEGER_ELEMENT_TYPE))
-    private Integer[] idUtenti;
+    private Integer[] idPersone;
     
     @Column(name = "per_tutti")
     private Boolean perTutti;
@@ -119,14 +119,14 @@ public class TemplateMessaggio implements Serializable {
     public TemplateMessaggio() {
     }
 
-    public TemplateMessaggio(String nomeTemplate, String titolo, String testo, String[] idApplicazioni, Integer[] idAziende, Integer[] idStrutture, Integer[] idUtenti, Boolean perTutti, LocalDateTime dataPubblicazione, String invasivita, String tipologia, Integer intervallo, LocalDateTime dataScadenza) {
+    public TemplateMessaggio(String nomeTemplate, String titolo, String testo, String[] idApplicazioni, Integer[] idAziende, Integer[] idStrutture, Integer[] idPersone, Boolean perTutti, LocalDateTime dataPubblicazione, String invasivita, String tipologia, Integer intervallo, LocalDateTime dataScadenza) {
         this.nomeTemplate = nomeTemplate;
         this.titolo = titolo;
         this.testo = testo;
         this.idApplicazioni = idApplicazioni;
         this.idAziende = idAziende;
         this.idStrutture = idStrutture;
-        this.idUtenti = idUtenti;
+        this.idPersone = idPersone;
         this.perTutti = perTutti;
         this.invasivita = invasivita;
         this.tipologia = tipologia;
@@ -189,12 +189,12 @@ public class TemplateMessaggio implements Serializable {
         this.idStrutture = idStrutture;
     }
 
-    public Integer[] getIdUtenti() {
-        return idUtenti;
+    public Integer[] getIdPersone() {
+        return idPersone;
     }
 
-    public void setIdUtenti(Integer[] idUtenti) {
-        this.idUtenti = idUtenti;
+    public void setIdPersone(Integer[] idPersone) {
+        this.idPersone = idPersone;
     }
 
     public Boolean getPerTutti() {
