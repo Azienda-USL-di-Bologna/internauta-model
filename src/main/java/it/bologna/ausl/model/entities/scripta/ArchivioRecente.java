c40a90b3e23d406ebf2ddcc6b56d426e84540599
mussi <m.mussi@nextsw.it>
2023-01-26 09:45:08 +0100

    archivio recente

---
+++ src/main/java/it/bologna/ausl/model/entities/scripta/ArchivioRecente.java
@@ -0,0 +1,141 @@
+package it.bologna.ausl.model.entities.scripta;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import it.bologna.ausl.model.entities.baborg.Persona;
+import it.nextsw.common.annotations.GenerateProjections;
+import java.time.ZonedDateTime;
+import javax.persistence.Basic;
+import javax.persistence.Cacheable;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Version;
+import javax.validation.constraints.NotNull;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+/**
+ *
+ * @author Matteo Next
+ */
+@Entity
+@Table(name = "archivi_recenti", catalog = "internauta", schema = "scripta")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Cacheable(false)
+@GenerateProjections({
+    "idArchivio"
+})
+@DynamicUpdate
+public class ArchivioRecente {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+
+    @JoinColumn(name = "id_persona", referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JsonBackReference(value = "idPersona")
+    private Persona idPersona;
+
+    @JoinColumn(name = "id_archivio", referencedColumnName = "id")
+    @ManyToOne(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JsonBackReference(value = "idArchivio")
+    private ArchivioDetail idArchivio;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXX'['VV']'")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXX'['VV']'")
+    @Column(name = "data_recentezza")
+    @Basic(optional = false)
+    @NotNull
+    private ZonedDateTime dataRecentezza;
+
+    @Version()
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXX'['VV']'")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSSXXX'['VV']'")
+    private ZonedDateTime version;
+
+    public ZonedDateTime getVersion() {
+        return version;
+    }
+
+    public void setVersion(ZonedDateTime version) {
+        this.version = version;
+    }
+
+    public ArchivioDetail getIdArchivio() {
+        return idArchivio;
+    }
+
+    public void setIdArchivio(ArchivioDetail idArchivio) {
+        this.idArchivio = idArchivio;
+    }
+
+    public ZonedDateTime getDataRecentezza() {
+        return dataRecentezza;
+    }
+
+    public void setDataRecentezza(ZonedDateTime dataRecentezza) {
+        this.dataRecentezza = dataRecentezza;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    public ArchivioRecente() {
+    }
+
+    public ArchivioRecente(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Persona getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(Persona idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ArchivioDiInteresse)) {
+            return false;
+        }
+        ArchivioRecente other = (ArchivioRecente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "it.bologna.ausl.model.entities.scripta.ArchivioRecente[ id=" + id + " ]";
+    }
+
+}
